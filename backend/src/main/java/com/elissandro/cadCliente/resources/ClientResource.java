082fbd3a27371e7e85f120813dd3ccbb549ebac7
Elissandro <elissandroa@hotmail.com>
2020-12-01 22:38:44 -0300

    ClientDTO

--- backend/src/main/java/com/elissandro/cadCliente/resources/ClientResource.java
+++ backend/src/main/java/com/elissandro/cadCliente/resources/ClientResource.java
@@ -1,25 +1,27 @@
 package com.elissandro.cadCliente.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.elissandro.cadCliente.entity.Client;
+import com.elissandro.cadCliente.dto.ClientDTO;
+import com.elissandro.cadCliente.services.ClientService;
 
 @RestController
 @RequestMapping(value = "/clients")
 public class ClientResource {
 	
+	@Autowired
+	private ClientService service;
+	
 	@GetMapping
-	public ResponseEntity<List<Client>> findAll(){
-		List<Client>list = new ArrayList<>();
-		list.add(new Client(1L, "João", "123456789", 1200.00, null, 2));
-		list.add(new Client(2L, "Maria", "9875679890", 1600.00, null, 1));
-		list.add(new Client(3L, "Pedro", "65498990890", 1400.00, null, 4));
+	public ResponseEntity<List<ClientDTO>> findAll(){
+		List<ClientDTO>list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
+
 }
